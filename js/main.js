eaa7977da391b9e3b26b9477aefcfdf9f77ef387
Jefferson Campos <foguinho.peruca@gmail.com>
2014-11-12 23:03:00 -0200

    Natal 2014
    * Limpeza geral da página;
    * Template jekyll para catálogo

--- js/main.js
+++ js/main.js
@@ -1,10 +1,10 @@
 // Menu
 $(document).ready(function() {
     $.fn.fullpage({
-			  menu: '#menu',
+		menu: '#menu',
         // anchors: ['home', 'atelier', 'natal2013', 'gostosuras', 'contato'],
         // slidesColor: ['rgb(198, 61, 15)', '#f2f2f2', '#4BBFC3', '#7BAABE', '#ccddff']
-        anchors: ['home', 'atelier', 'natal2013', 'gostosuras', 'contato'],
-        slidesColor: ['#C63D0F', '#f2f2f2', '#4BBFC3', '#7BAABE', '#ccddff']
+        anchors: ['home', 'natal2014'],
+        slidesColor: ['#C63D0F', '#7BAABE']
     });
 });
