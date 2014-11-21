551be7b27f57f4af54136bbc0dd2e0ce7da29bc1
Jefferson Campos <foguinho.peruca@gmail.com>
2014-11-21 02:12:02 -0200

    Home
    Adicionada nova cor, mais próxima da usada no cartão

--- js/main.js
+++ js/main.js
@@ -1,10 +1,9 @@
-// Menu
 $(document).ready(function() {
     $.fn.fullpage({
 		menu: '#menu',
         // anchors: ['home', 'atelier', 'natal2013', 'gostosuras', 'contato'],
         // slidesColor: ['rgb(198, 61, 15)', '#f2f2f2', '#4BBFC3', '#7BAABE', '#ccddff']
         anchors: ['home', 'natal2014'],
-        slidesColor: ['#754719', '#C63D0F']
+        slidesColor: ['#663300', '#C63D0F']
     });
 });
