b66aa393aaa7dc16ea5c304fae5c1b0deb06e82f
Jefferson <foguinho.peruca@gmail.com>
2014-12-03 10:34:39 -0200

    Background Color
    Padronizada

--- js/main.js
+++ js/main.js
@@ -4,6 +4,6 @@ $(document).ready(function() {
         // anchors: ['home', 'atelier', 'natal2013', 'gostosuras', 'contato'],
         // slidesColor: ['rgb(198, 61, 15)', '#f2f2f2', '#4BBFC3', '#7BAABE', '#ccddff']
         anchors: ['home', 'natal2014'],
-        slidesColor: ['#663300', '#C63D0F']
+        slidesColor: ['#663300', '#663300']
     });
 });
